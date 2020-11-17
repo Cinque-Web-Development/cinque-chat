0b12213999a38cea8ed20b7ba7a1e450242970e1
Samuel Trahan <samtrahan91@gmail.com>
2020-11-17 14:43:15 -0600

    chat component set up and rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import Chat from "./components/Chat/Chat";
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Chat />
+    </>
+  );
 }
-
